44c25e9c01698f9e2f0b7dd6d3c4f1d36d454019
JC Heyer <4446535+jcheyer@users.noreply.github.com>
2021-12-10 13:29:22 +0100

    d10p1 done

--- challenges/day10.go
+++ challenges/day10.go
@@ -2,6 +2,7 @@ package challenges
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/jcheyer/aoc2021/lib"
 )
@@ -25,11 +26,59 @@ func (d *Day10) Load(file string) error {
 }
 
 func (d *Day10) Part1() string {
-
-	return fmt.Sprintf("%d", 0)
+	score := 0
+	for _, line := range d.rawInput {
+		score = score + checkLine(line)
+	}
+	return fmt.Sprintf("%d", score)
 }
 
 func (d *Day10) Part2() string {
 
 	return fmt.Sprintf("%d", 0)
+}
+
+func checkLine(line string) int {
+	s := lib.NewStack()
+	for _, r := range line {
+		switch r {
+		case '(', '[', '{', '<':
+			s.Push(r)
+		case ')':
+			c, err := s.Pop()
+			if err != nil {
+				log.Panicf("stack errorr %c in %s", r, line)
+			}
+			if c != '(' {
+				return 3
+			}
+		case ']':
+			c, err := s.Pop()
+			if err != nil {
+				log.Panicf("stack errorr %c in %s", r, line)
+			}
+			if c != '[' {
+				return 57
+			}
+		case '}':
+			c, err := s.Pop()
+			if err != nil {
+				log.Panicf("stack errorr %c in %s", r, line)
+			}
+			if c != '{' {
+				return 1197
+			}
+		case '>':
+			c, err := s.Pop()
+			if err != nil {
+				log.Panicf("stack error %c in %s", r, line)
+			}
+			if c != '<' {
+				return 25137
+			}
+		default:
+			log.Panicf("bad char %c in %s", r, line)
+		}
+	}
+	return 0
 }
---
+++ challenges/day10_test.go
@@ -0,0 +1,15 @@
+package challenges
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay10(t *testing.T) {
+	d := Day10{}
+	err := d.Load("testfiles/d10demo.txt")
+	assert.NoError(t, err)
+	assert.Len(t, d.rawInput, 10)
+	assert.Equal(t, "26397", d.Part1())
+}
